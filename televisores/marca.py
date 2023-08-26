e30de48de2051a84a825bba6e3cffb1636b3da73
Moscotrofio <indarttomas@gmail.com>
2023-08-25 21:44:24 -0500

    Update marca.py

--- televisores/marca.py
+++ televisores/marca.py
@@ -5,6 +5,6 @@ class Marca:
     def getNombre(self):
         return self.nombre
     
-    def setNombre(nombre):
-        Marca.nombre = nombre
+    def setNombre(self, nombre):
+        self.nombre = nombre
     
