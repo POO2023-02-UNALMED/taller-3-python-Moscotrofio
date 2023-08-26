168204879f64d445bf24d746b22a2120f8929164
Moscotrofio <indarttomas@gmail.com>
2023-08-25 21:55:17 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -3,7 +3,7 @@ class TV:
     def __init__(self, marca, estado):
         self._marca = marca
         self._estado = estado
-        self.canal = 1
+        self._canal = 1
         self._volumen = 1
         self._precio = 500
         self._control = None
